46e4ce273a5be44833ef5eee8f28ff49c3869db0
vibalmeida_meli <vinicius.balmeida@mercadolivre.com>
2025-08-11 15:47:01 -0300

    first commit

---
+++ src/main/java/com/teamcubation/viacepbot/ViacepBotApplication.java
@@ -0,0 +1,13 @@
+package com.teamcubation.viacepbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViacepBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViacepBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamcubation/viacepbot/ViacepBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcubation.viacepbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViacepBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
